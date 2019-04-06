18c38990973b254780afeed69ce219e0cb7b880a
sterios <sterios.taskudis@gmail.com>
2019-04-06 21:21:35 +0300

    fix: Create Parallax component.

---
+++ src/components/custom/Paralax.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-parallax
+            dark
+            height="350"
+            v-bind:src="require('../../../public/img/header-image.jpeg')"
+    >
+        <v-layout
+                align-center
+                column
+                justify-center
+        >
+        </v-layout>
+    </v-parallax>
+</template>
+
+<script>
+    export default {
+        name: 'paralax',
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped lang="stylus">
+</style>
